143363828be1e8696d7efa4c6bbb4beba0a4be01
Arthur Chen <crh0716@gmail.com>
2013-12-21 19:31:01 +0800

    1. Add sound panel 2. Add Utils

--- test_apps/settings2/js/panels/DisplayPanel.js
+++ test_apps/settings2/js/panels/DisplayPanel.js
@@ -1,20 +1,6 @@
-define('panels/DisplayPanel', ['modules/Panel', 'modules/Wallpaper'],
-  function (Panel, Wallpaper) {
-    function loadJSON(href, callback) {
-      if (!callback)
-        return;
-      var xhr = new XMLHttpRequest();
-      xhr.onerror = function() {
-        console.error('Failed to fetch file: ' + href, xhr.statusText);
-      };
-      xhr.onload = function() {
-        callback(xhr.response);
-      };
-      xhr.open('GET', href, true); // async
-      xhr.responseType = 'json';
-      xhr.send();
-    }
-
+define('panels/DisplayPanel',
+  ['modules/Panel', 'modules/Wallpaper', 'modules/Utils'],
+  function(Panel, Wallpaper, Utils) {
     var ready = function(rootElement, options) {
       var panel = rootElement;
       var settings = navigator.mozSettings;
@@ -28,23 +14,24 @@ define('panels/DisplayPanel', ['modules/Panel', 'modules/Wallpaper'],
       // hide "Adjust automatically" if there's no ambient light sensor --
       // until bug 876496 is fixed, we have to read the `sensors.json' file to
       // be sure this ambient light sensor is enabled.
-      loadJSON('/resources/sensors.json', function loadSensors(activeSensors) {
-        if (activeSensors.ambientLight) { // I can haz ambient light sensor
-          autoBrightness.hidden = false;
-          settings.addObserver(autoBrightnessSetting, function(event) {
-            manualBrightness.hidden = event.settingValue;
-          });
-          var req = settings.createLock().get(autoBrightnessSetting);
-          req.onsuccess = function brightness_onsuccess() {
-            manualBrightness.hidden = req.result[autoBrightnessSetting];
-          };
-        } else { // no ambient light sensor: force manual brightness setting
-          autoBrightness.hidden = true;
-          manualBrightness.hidden = false;
-          var cset = {};
-          cset[autoBrightnessSetting] = false;
-          settings.createLock().set(cset);
-        }
+      Utils.loadJSON('/resources/sensors.json',
+        function loadSensors(activeSensors) {
+          if (activeSensors.ambientLight) { // I can haz ambient light sensor
+            autoBrightness.hidden = false;
+            settings.addObserver(autoBrightnessSetting, function(event) {
+              manualBrightness.hidden = event.settingValue;
+            });
+            var req = settings.createLock().get(autoBrightnessSetting);
+            req.onsuccess = function brightness_onsuccess() {
+              manualBrightness.hidden = req.result[autoBrightnessSetting];
+            };
+          } else { // no ambient light sensor: force manual brightness setting
+            autoBrightness.hidden = true;
+            manualBrightness.hidden = false;
+            var cset = {};
+            cset[autoBrightnessSetting] = false;
+            settings.createLock().set(cset);
+          }
       });
     };
 
