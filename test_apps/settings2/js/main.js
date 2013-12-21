7ee9bf34e539b1c57580326bf7341a1a1b50f9c3
Arthur Chen <crh0716@gmail.com>
2013-12-22 00:43:57 +0800

    1. Add activity handler 2. Separate the logic of init and ready in panels

--- test_apps/settings2/js/main.js
+++ test_apps/settings2/js/main.js
@@ -1,13 +1,58 @@
-require({
-  paths: {
-    'shared': '../shared'
-  }
-});
+'strict';
 
-require([
-  'modules/SettingsService'
-], function (SettingsService) {
-  navigator.mozL10n.ready(function() {
-    SettingsService.navigate('root');
+(function() {
+  var _launchPanel = 'root';
+  var _webActivityHandler = function(activityRequest) {
+    dump("=== activity handler");
+    var name = activityRequest.source.name;
+    switch (name) {
+      case 'configure':
+        section = activityRequest.source.data.section;
+
+        if (!section) {
+          // If there isn't a section specified,
+          // simply show ourselve without making ourselves a dialog.
+        }
+
+        // Validate if the section exists
+        var sectionElement = document.getElementById(section);
+        if (!sectionElement || sectionElement.tagName !== 'SECTION') {
+          var msg = 'Trying to open an non-existent section: ' + section;
+          console.warn(msg);
+          activityRequest.postError(msg);
+          return;
+        }
+
+        // Go to that section
+        _launchPanel = section;
+        break;
+    }
+
+    // Mark the desired panel as a dialog
+    /*if (Settings._currentActivity !== null) {
+      var domSection = document.getElementById(section);
+      domSection.dataset.dialog = true;
+      document.addEventListener('visibilitychange',
+        Settings.visibilityHandler);
+    }*/
+  };
+
+  // Register the handler as soon as possible.
+  navigator.mozSetMessageHandler('activity', _webActivityHandler);
+
+  require({
+    paths: {
+      'shared': '../shared'
+    }
   });
-});
+
+  require([
+    'modules/SettingsService'
+  ], function (SettingsService) {
+    if (!navigator.mozSettings || !navigator.mozSetMessageHandler) {
+      return;
+    }
+
+    SettingsService.navigate(_launchPanel);
+  });
+})();
