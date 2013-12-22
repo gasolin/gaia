e5ace97b66a01950761b9ebc0cd4a295ad258d5f
Arthur Chen <crh0716@gmail.com>
2013-12-22 20:34:31 +0800

    1. Remove listeners when done and add them back when ready

--- test_apps/settings2/js/panels/BatteryPanel.js
+++ test_apps/settings2/js/panels/BatteryPanel.js
@@ -1,21 +1,25 @@
 define('panels/BatteryPanel', ['modules/Panel', 'modules/Battery'],
   function(Panel, Battery) {
-    var _init = function(rootElement, options) {
-      _initBatteryText(rootElement);
+    var _batteryLevelText = null;
+    var _refreshText = function() {
+      navigator.mozL10n.localize(_batteryLevelText,
+                                 'batteryLevel-percent-' + Battery.state,
+                                 { level: Battery.level });
     };
 
-    var _initBatteryText = function(rootElement) {
-      var _batteryLevelText = rootElement.querySelector('#battery-level *');
-      var _refreshText = function() {
-        navigator.mozL10n.localize(_batteryLevelText,
-                                   'batteryLevel-percent-' + Battery.state,
-                                   { level: Battery.level });
-      };
+    var _init = function(rootElement, options) {
+      _batteryLevelText = rootElement.querySelector('#battery-level *');
+    };
 
+    var _ready = function(rootElement, options) {
       Battery.observe('level', _refreshText);
       Battery.observe('state', _refreshText);
       _refreshText();
     };
 
-    return Panel(_init);
+    var _done = function() {
+      Battery.unobserve(_refreshText);
+    };
+
+    return Panel(_init, null, _ready, _done);
 });
