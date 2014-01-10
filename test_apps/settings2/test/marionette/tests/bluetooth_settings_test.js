1e3e4b142277e22a0c12b80ff37d3aaa77322de7
Evan Xd <itoyxd@gmail.com>
2014-01-10 15:44:54 +0800

    Skip all marionette tests from Settings1 app.

--- test_apps/settings2/test/marionette/tests/bluetooth_settings_test.js
+++ test_apps/settings2/test/marionette/tests/bluetooth_settings_test.js
@@ -1,30 +1,30 @@
-var Settings = require('../app/app'),
-    assert = require('assert');
+// var Settings = require('../app/app'),
+//     assert = require('assert');
 
-marionette('manipulate bluetooth settings', function() {
-  var client = marionette.client();
-  var settingsApp;
+// marionette('manipulate bluetooth settings', function() {
+//   var client = marionette.client();
+//   var settingsApp;
 
-  setup(function() {
-    settingsApp = new Settings(client);
-    settingsApp.launch();
-    // Navigate to the Bluetooth menu
-    bluetoothPanel = settingsApp.bluetoothPanel;
-  });
+//   setup(function() {
+//     settingsApp = new Settings(client);
+//     settingsApp.launch();
+//     // Navigate to the Bluetooth menu
+//     bluetoothPanel = settingsApp.bluetoothPanel;
+//   });
 
-  test('check bluetooth initial state', function() {
-    assert.ok(
-      !bluetoothPanel.isBluetoothEnabled,
-      'bluetooth is disabled by default'
-    );
-  });
+//   test('check bluetooth initial state', function() {
+//     assert.ok(
+//       !bluetoothPanel.isBluetoothEnabled,
+//       'bluetooth is disabled by default'
+//     );
+//   });
 
-  test('enable bluetooth', function() {
-    bluetoothPanel.enableBluetooth();
-    assert.ok(
-      bluetoothPanel.isBluetoothEnabled,
-      'bluetooth has been enabled'
-    );
-  });
+//   test('enable bluetooth', function() {
+//     bluetoothPanel.enableBluetooth();
+//     assert.ok(
+//       bluetoothPanel.isBluetoothEnabled,
+//       'bluetooth has been enabled'
+//     );
+//   });
 
-});
+// });
